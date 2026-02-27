bbeee82713b9003a768c1241fa47f87081db0a69
jongmin <bergi4025@gmail.com>
2026-02-27 13:06:18 +0900

    #1 feat: 모달 컴파운드 컴포넌트 도입 및 버튼 사이즈/타이포 토큰 정리

--- src/shared/pages/TestPage.tsx
+++ src/shared/pages/TestPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "../components/Button";
+import Modal from "../components/Modal";
 
 function TestPage() {
   const [isLoading, setIsLoading] = useState(false);
@@ -10,12 +11,28 @@ function TestPage() {
 
   return (
     <div>
-      <Button onClick={handleClick}>확인</Button>
-      <Button isLoading={isLoading}>확인ㅇㄴㅁㅇㄴㅁㅇㅁ</Button>
-      <Button variant="danger" isLoading={isLoading}>
+      <Modal>
+        <Modal.Header>삭제 버튼 있는 모달</Modal.Header>
+        <Modal.Description>
+          삭제 버튼이 있는 Modal이고 여기에는 Modal에 대한 설명이 들어가요
+        </Modal.Description>
+        <Modal.ButtonLayout>
+          <Button variant="danger" size="modal">
+            삭제
+          </Button>
+          <Modal.Cancle />
+        </Modal.ButtonLayout>
+      </Modal>
+      <Button size="primary" onClick={handleClick}>
         확인
       </Button>
-      <Button className="py-2.5 text-[15px]" isLoading={isLoading}>
+      <Button size="primary" isLoading={isLoading}>
+        확인ㅇㄴㅁㅇㄴㅁㅇㅁ
+      </Button>
+      <Button size="primary" variant="danger" isLoading={isLoading}>
+        확인
+      </Button>
+      <Button size="large" isLoading={isLoading}>
         확인
       </Button>
     </div>
