cdf8d711b5d50fb0c3a1037b75bb157c4a60a218
jongmin <bergi4025@gmail.com>
2026-02-27 14:56:18 +0900

    feat: SearchBar 컴포넌트 추가 및 SVGR 아이콘 연동

--- src/shared/pages/TestPage.tsx
+++ src/shared/pages/TestPage.tsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import Button from "../components/Button";
-import Modal from "../components/Modal";
+import SerachBar from "../components/SerachBar";
+// import Modal from "../components/Modal";
 
 function TestPage() {
+  const [value, setValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = () => {
     setIsLoading(true);
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((prev) => (prev === e.target.value ? prev : e.target.value));
+  };
+
   return (
     <div>
-      <Modal>
+      <SerachBar value={value} onChange={handleChange} />
+      {/* <Modal>
         <Modal.Header>삭제 버튼 있는 모달</Modal.Header>
         <Modal.Description>
           삭제 버튼이 있는 Modal이고 여기에는 Modal에 대한 설명이 들어가요
@@ -22,7 +29,7 @@ function TestPage() {
           </Button>
           <Modal.Cancle />
         </Modal.ButtonLayout>
-      </Modal>
+      </Modal> */}
       <Button size="primary" onClick={handleClick}>
         확인
       </Button>
