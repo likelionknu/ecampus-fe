e6cafbd971f1a2fce6b3bd63813b791fe928031d
pyw424 <parkyw0424@naver.com>
2026-03-03 14:03:16 +0900

    #11 ui: 자료 상세 조회 페이지 추가

---
+++ src/user/domains/session/pages/UserSessionFilesViewPage.tsx
@@ -0,0 +1,9 @@
+function UserSessionFilesViewPage() {
+  return (
+    <div className="flex w-full max-w-251 flex-col gap-5 px-8 pt-7">
+      자료 상세 페이지
+    </div>
+  );
+}
+
+export default UserSessionFilesViewPage;
