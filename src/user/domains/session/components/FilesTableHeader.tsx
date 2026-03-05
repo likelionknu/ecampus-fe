a4163eb865500e97a615cf07ee6ba67616f8c290
pyw424 <parkyw0424@naver.com>
2026-03-06 06:23:24 +0900

    #11 ui: 자료 페이지 리스트 생성

--- src/user/domains/session/components/FilesTableHeader.tsx
+++ src/user/domains/session/components/FilesTableHeader.tsx
@@ -2,17 +2,15 @@ import TableHeaderLabel from "@/shared/components/table/TableHeaderLabel";
 
 function FilesTableHeader() {
   return (
-    <>
-      <div className="flex gap-7">
-        <TableHeaderLabel>ID</TableHeaderLabel>
-        <TableHeaderLabel>자료 명</TableHeaderLabel>
-      </div>
-      <div className="flex gap-14.5">
-        <TableHeaderLabel className="mr-19">등록일</TableHeaderLabel>
-        <TableHeaderLabel>등록한 사용자</TableHeaderLabel>
-      </div>
-    </>
+    <div
+      className="grid w-full items-center gap-x-10"
+      style={{ gridTemplateColumns: "20px minmax(0,1fr) 180px 120px" }}
+    >
+      <TableHeaderLabel className="text-center">ID</TableHeaderLabel>
+      <TableHeaderLabel>자료 명</TableHeaderLabel>
+      <TableHeaderLabel className="text-center">등록일</TableHeaderLabel>
+      <TableHeaderLabel className="text-center">등록한 사용자</TableHeaderLabel>
+    </div>
   );
 }
-
 export default FilesTableHeader;
