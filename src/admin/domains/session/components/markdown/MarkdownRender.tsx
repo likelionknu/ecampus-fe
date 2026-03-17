999d7c0918bafaf153f8ce0c5a0a42ba5066c4cc
pyw424 <parkyw0424@naver.com>
2026-03-17 19:28:03 +0900

    #48 ui: 마크다운 공통 컴포넌트 생성

---
+++ src/admin/domains/session/components/markdown/MarkdownRender.tsx
@@ -0,0 +1,23 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import { markdownComponents } from "./MarkdownComponents";
+
+
+interface Props {
+  content: string;
+}
+
+export default function MarkdownRenderer({ content }: Props) {
+  return (
+    <div className="prose max-w-none">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeHighlight]}
+        components={markdownComponents}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+}
